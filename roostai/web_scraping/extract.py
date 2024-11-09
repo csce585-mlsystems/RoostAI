57144ffb9dd61bf79a2293c4611c1da441bc5136
Vansh Nagpal <2002vansh@gmail.com>
2024-11-09 13:31:44 -0500

    extracting pdf main text

--- roostai/web_scraping/extract.py
+++ roostai/web_scraping/extract.py
@@ -1,7 +1,10 @@
 import os
 from bs4 import BeautifulSoup
 import shutil
-
+import json
+import requests
+import io
+from PyPDF2 import PdfReader
 
 def extract_main_text(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -36,12 +39,30 @@ def extract_main_text(html_content):
     #     return ""  # Return empty string if <main> tag is not found
 
 
+def extract_pdf_text(url):
+    # downloading pdf
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # converting pdf to binary stream
+    pdf_file = io.BytesIO(response.content)
+
+    # pdf reader object from file
+    pdf_reader = PdfReader(pdf_file)
+
+    # extract text
+    text = ''
+    for page in pdf_reader.pages:
+        text += page.extract_text() + " "
+    return text
+
+
 def save_text_to_file(text, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(text)
 
 
-def process_html_files(input_dir, output_dir):
+def process_files(input_dir, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -51,18 +72,28 @@ def process_html_files(input_dir, output_dir):
           dest_file = os.path.join(output_dir, file_name)
           shutil.copy2(source_file, dest_file) 
           continue
-        
+              
         input_file_path = os.path.join(input_dir, file_name)
         
+        # extracting url
+        metadata_file_name = file_name.replace('scraped_html_', 'metadata_')
+        metadata = os.path.join(input_dir, metadata_file_name)
+        with open(metadata, 'r', encoding='utf-8') as file:
+          metadata = json.load(file)
+          url = metadata['source_url']
+        
+        if '.pdf' in url:
+          # extract text from pdf url
+          main_text = extract_pdf_text(url)         
+        else:
+          with open(input_file_path, 'r', encoding='utf-8') as html_file:
+              html_content = html_file.read()
+          main_text = extract_main_text(html_content)
+        
+        # save main text to output file
         file_name_without_extension, _= file_name.split('.')
         output_file_path = os.path.join(output_dir, f'{file_name_without_extension}.txt')
-
-        with open(input_file_path, 'r', encoding='utf-8') as html_file:
-            html_content = html_file.read()
-
-        main_text = extract_main_text(html_content)
         save_text_to_file(main_text, output_file_path)
-
         print(f"Processed {file_name} and saved to {output_file_path}")
 
 
@@ -70,5 +101,5 @@ if __name__ == "__main__":
   # Example usage
   input_directory = '/home/cc/collected_data'
   output_directory = '/home/cc/extracted_data'
-  process_html_files(input_directory, output_directory)
+  process_files(input_directory, output_directory)
   print("Main text extracted!")
