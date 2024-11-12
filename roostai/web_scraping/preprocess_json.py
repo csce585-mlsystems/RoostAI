d1f9ea1ffa9aec8c1307777171137c5f55391137
Vansh Nagpal <2002vansh@gmail.com>
2024-11-12 14:12:03 -0500

    json preprocessing function

---
+++ roostai/web_scraping/preprocess_json.py
@@ -0,0 +1,25 @@
+import json
+import re
+
+
+def preprocess_json(file_path):
+    # read uncleaned file
+    with open('test.json', 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    # replace all pesky \uXXXX characters
+    modified_content = re.sub(r'\\u[0-9A-Fa-f]{4}', ' ', content)
+    
+    # shrink all unnecessary whitespaces (\t, \n, etc.)
+    json_dict = json.loads(modified_content)
+
+    def shrink_whitespaces(str_): return " ".join(str_.split())
+
+    json_dict['main_text'] = shrink_whitespaces(json_dict['main_text'])
+
+    json_dict['chunks'] = [shrink_whitespaces(
+        chunk) for chunk in json_dict['chunks']]
+
+    # write cleaned file
+    with open('test.json', 'w', ) as file:
+        file.write(modified_content)
