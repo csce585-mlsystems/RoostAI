9c8b10e63738a65011fdcae5b0bab657c9b4ab94
Vansh Nagpal <2002vansh@gmail.com>
2024-12-03 22:46:17 -0500

    black reformatting

--- roostai/chunk_experiment/count_chunks.py
+++ roostai/chunk_experiment/count_chunks.py
@@ -3,11 +3,12 @@ import os
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 import pandas as pd
-chunk_dir = '/home/cc/chunks_and_metadata'
+
+chunk_dir = "/home/cc/chunks_and_metadata"
 chunk_files = [os.path.join(chunk_dir, file) for file in os.listdir(chunk_dir)]
 
 # Load the model
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 
 def count_tokens(text):
@@ -18,9 +19,9 @@ def count_tokens(text):
 
 token_counts = []
 for file in tqdm(chunk_files):
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         file = json.load(f)
-    chunks = file['chunks']
+    chunks = file["chunks"]
     token_counts.extend([count_tokens(chunk) for chunk in chunks])
 
 token_counts = pd.Series(token_counts)
