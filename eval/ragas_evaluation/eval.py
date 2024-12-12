63e1b311ca7eed1285a2e751d56bf6fb2b08f703
Vansh Nagpal <2002vansh@gmail.com>
2024-12-12 00:07:39 -0500

    updating evaluation script to evaluate rag output

--- eval/ragas_evaluation/eval.py
+++ eval/ragas_evaluation/eval.py
@@ -1,3 +1,9 @@
+import logging
+import asyncio
+import os
+import json
+from tqdm import tqdm
+from statistics import mean
 from ragas import SingleTurnSample
 from ragas.metrics import LLMContextPrecisionWithReference, LLMContextRecall, FaithfulnesswithHHEM, NoiseSensitivity, ResponseRelevancy
 
@@ -7,13 +13,22 @@ context_recall = LLMContextRecall()
 faithfulness = FaithfulnesswithHHEM()
 noise_sensitivity = NoiseSensitivity()
 
+# initialize logger
+logging.basicConfig(
+    level=logging.INFO,  # WARNING, ERROR, CRITICAL messages will be logged
+    # Timestamp - (Warning/Error/Critical) - STDOUT
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+# sets the name of the logger to be the name of the module
+logger = logging.getLogger(__name__)
+
 
 async def rag_eval(query_info: dict):
     """
-    query_info = {'query': str, 'ground_truth': str, 'rag_response': str, 'contexts': List[str]}
+    query_info = {'query': str, 'ground_truth': str, 'response': str, 'contexts': List[str]}
     """
     sample = SingleTurnSample(
-        user_input=query_info['query'], reference=query_info['ground_truth'], response=query_info['rag_response'], retrieved_contexts=query_info['contexts'])
+        user_input=query_info['query'], reference=query_info['ground_truth'], response=query_info['response'], retrieved_contexts=query_info['contexts'])
 
     context_precision_score = await context_precision.single_turn_ascore(sample)
     context_recall_score = await context_precision.single_turn_ascore(sample)
@@ -21,6 +36,51 @@ async def rag_eval(query_info: dict):
     noise_sensitivity_score = await noise_sensitivity.single_turn_ascore(sample)
 
     # add previous evals
-    
+
     evals = {'context_precision': context_precision_score, 'context_recall': context_recall_score,
              'faithfulness': faithfulness_score, 'noise_sensitivty': noise_sensitivity_score}
+
+    return evals
+
+
+async def main():
+    rag_response_dir = '/home/cc/RoostAI/eval/ragas_evaluation/data/results'
+    rag_response_dirs = [os.path.join(
+        rag_response_dir, folder) for folder in os.listdir(rag_response_dir)]
+    rag_response_dirs = [os.path.join(folder, os.listdir(
+        folder)[0]) for folder in rag_response_dirs]
+
+    failures = {(
+        "I don't have any relevant information to answer your question. "
+        "Please try asking something else about USC."
+    ), (
+        "I don't have enough confident information to provide a good answer. "
+        "Please try rephrasing your question."
+    ), "An error occurred processing your query."}
+
+    for response_dir in tqdm(rag_response_dirs, 'Chunking Strategy')
+        logger.info(f'Evaluating {response_dir}')
+        context_precision_scores = []
+        context_recall_scores = []
+        faithfulness_scores = []
+        noise_sensitivity_scores = []
+        with open(os.path.join(response_dir, 'detailed_results.json'), 'r') as f:
+            results: list = json.load(f)
+            for res in tqdm(results, 'Responses'):
+                if res['response'] in failures:
+                    continue
+                scores = await rag_eval(res)
+                context_precision_scores.append(scores['context_precision'])
+                context_recall_scores.append(scores['context_recall'])
+                faithfulness_scores.append(scores['faithfulness'])
+                noise_sensitivity_scores.append(scores['noise_sensitivty'])
+
+        mean_scores = {'context_precision': mean(context_precision_scores), 'context_recall': mean(
+            context_recall_scores), 'faithfulness': mean(faithfulness_scores), 'noise_sensitivity': mean(noise_sensitivity_scores)}
+
+        with open(os.path.join(response_dir, 'rag_scores.json'), 'w') as f:
+            json.dump(mean_scores, f)
+
+
+if __name__ == '__main__':
+    asyncio.run(main)
